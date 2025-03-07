7b144e7f40552d19438cb515d436c294cc48739d
edard3v <edard3v@gmail.com>
2025-03-07 11:04:28 -0500

    refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  // vite: {
-  //   build: {
-  //     assetsInlineLimit: 0,
-  //   },
-  // },
+  integrations: [icon()],
+  vite: {
+    build: {
+      assetsInlineLimit: 0,
+    },
+  },
 });
--- src/views/Home/FavoriteAnimes/animes.ts
+++ src/views/Home/FavoriteAnimes/animes.ts
@@ -9,25 +9,35 @@ export const ANIMES = [
     id: crypto.randomUUID(),
     name: "Darker than Black",
     img: darker_than_black,
+    synopsis:
+      "Hace diez años, apareció la 'Puerta del Infierno' en Tokio, alterando el cielo y trayendo a los 'Contratistas', individuos con habilidades sobrenaturales que deben pagar un precio por usarlas. Hei, un misterioso Contratista conocido como 'BK201', trabaja en la sombra para una organización secreta mientras oculta su verdadero propósito.",
   },
   {
     id: crypto.randomUUID(),
     name: "Monogatari",
     img: monogatari,
+    synopsis:
+      "Koyomi Araragi, un estudiante de secundaria, sobrevive a un ataque vampírico y mantiene ciertas habilidades inhumanas. A medida que ayuda a chicas con problemas relacionados con entidades sobrenaturales, se adentra en un mundo de maldiciones, deidades y misterios mientras enfrenta su propia naturaleza.",
   },
   {
     id: crypto.randomUUID(),
     name: "Monster",
     img: monster,
+    synopsis:
+      "El Dr. Kenzo Tenma es un neurocirujano que salva la vida de un niño en lugar de un político influyente. Años después, descubre que aquel niño se ha convertido en un asesino en serie demente. Consumido por la culpa, Tenma inicia una cacería para detener al monstruo que creó.",
   },
   {
     id: crypto.randomUUID(),
     name: "Oshi no Ko",
     img: oshi_no_ko,
+    synopsis:
+      "Gorou Amamiya, un ginecólogo fanático de la idol Ai Hoshino, es asesinado y reencarna como su hijo, Aqua. Junto a su hermana gemela Ruby, descubre la oscura verdad detrás de la industria del entretenimiento y jura vengarse del hombre responsable de la muerte de su madre.",
   },
   {
     id: crypto.randomUUID(),
     name: "Classroom of the Elite",
     img: classroom_of_the_elite,
+    synopsis:
+      "Kiyotaka Ayanokouji ingresa a la prestigiosa Escuela de Educación Avanzada, donde los estudiantes son clasificados por rendimiento. A pesar de estar en la Clase D, la más baja, su fría inteligencia y habilidades ocultas le permiten manipular su entorno y desafiar el sistema.",
   },
 ];
