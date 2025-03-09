2de73cf1314bc496915298ca2104b65f434d3a7b
edard3v <edard3v@gmail.com>
2025-03-08 20:36:30 -0500

    web-componen de mrd 0.0

--- src/views/Anime/AnimeIsland/AnimeById/AnimeById.tsx
+++ src/views/Anime/AnimeIsland/AnimeById/AnimeById.tsx
@@ -4,6 +4,7 @@ import { get_anime_id } from "./get_anime_id";
 import { get_anime_by_id_full_fetch } from "./get_anime_by_id_full_fetch/get_anime_by_id_full_fetch";
 import ErrorComp from "@solid/components/errors/ErrorComp/ErrorComp";
 import Loading from "@solid/components/loaders/Loading/Loading";
+import "@justinribeiro/lite-youtube";
 
 export default function AnimeById() {
   const anime_id = get_anime_id();
@@ -12,11 +13,27 @@ export default function AnimeById() {
     queryFn: ({ signal }) => get_anime_by_id_full_fetch({ signal, id: anime_id }),
   }));
 
+  const youtube_id = () => query.data?.data.trailer.youtube_id;
+
   return (
     <div class={css.anime_by_id}>
       {query.isError && <ErrorComp />}
       {query.isLoading && <Loading />}
-      {query.isSuccess && <div>{query.data?.data.title}</div>}
+
+      {query.isSuccess && (
+        <>
+          <span>{youtube_id()}</span>
+          <lite-youtube class={css.lite} videoid={"1yXa8MAmocQ"}></lite-youtube>
+        </>
+      )}
     </div>
   );
+}
+
+declare module "solid-js" {
+  namespace JSX {
+    interface IntrinsicElements {
+      "lite-youtube": any;
+    }
+  }
 }
