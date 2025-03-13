87025525c65d26f7288cd5e2a3f63465c9cb2f3b
edard3v <edard3v@gmail.com>
2025-03-13 16:42:43 -0500

    funciona

--- src/svelte/components/shared/SearchAnimes/SearchAnimes.svelte
+++ src/svelte/components/shared/SearchAnimes/SearchAnimes.svelte
@@ -2,9 +2,9 @@
   import { search_animes_store } from "./search_animes_store";
   import css from "./SearchAnimes.module.css";
 
-  const search_anime = $derived($search_animes_store);
+  const search_animes = $derived($search_animes_store);
 </script>
 
-<div class={[css.search, !search_anime && "hidden"]}>
+<div class={[css.search, !search_animes.is_visible && "hidden"]}>
   <input type="text" placeholder="Buscar" />
 </div>
--- src/svelte/components/shared/SearchAnimes/search_animes_store.ts
+++ src/svelte/components/shared/SearchAnimes/search_animes_store.ts
@@ -1,5 +1,13 @@
 import { atom } from "nanostores";
 
-export const search_animes_store = atom(false);
+export const search_animes_store = atom({
+  is_visible: false,
+});
 
-export const search_animes_store_toggle = () => search_animes_store.set(!search_animes_store.get());
+export const search_animes_store_toggle_is_visible = () => {
+  const store = search_animes_store.get();
+  search_animes_store.set({
+    ...store,
+    is_visible: !store.is_visible,
+  });
+};
