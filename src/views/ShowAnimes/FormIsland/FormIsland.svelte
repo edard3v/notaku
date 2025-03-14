baf29f3f8f78d7528b808bc6364a77ca919e71e6
edard3v <edard3v@gmail.com>
2025-03-14 15:23:15 -0500

    xd

---
+++ src/views/ShowAnimes/FormIsland/FormIsland.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Search from "@svelte/components/inputs/Search/Search.svelte";
+  import Btn from "@svelte/components/buttons/Btn/Btn.svelte";
+</script>
+
+<form>
+  <Search name="search" />
+  <input type="password" name="password" placeholder="Contraseña" />
+  <Btn>Enviar</Btn>
+</form>
+
+<style>
+  form {
+    max-width: 300px;
+    margin-left: auto;
+    margin-right: auto;
+    border: var(--br);
+    padding: var(--xl);
+    display: grid;
+    gap: var(--lg);
+  }
+</style>
