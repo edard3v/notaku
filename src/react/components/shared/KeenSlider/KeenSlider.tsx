c40f777ae4535b488789c05fd97f56f127b716df
edard3v <edard3v@gmail.com>
2025-03-17 22:52:29 -0500

    mejorando KeenSlider

--- src/react/components/shared/KeenSlider/KeenSlider.tsx
+++ src/react/components/shared/KeenSlider/KeenSlider.tsx
@@ -4,15 +4,24 @@ import { useKeenSlider } from "keen-slider/react";
 import React from "react";
 
 export default function KeenSlider(props: Props) {
-  const { className, children, options } = props;
+  const { className, children, options, ...rest } = props;
 
   const [container_ref, _slider] = useKeenSlider({
     slides: { perView: 1, ...options },
   });
 
+  const cloned_children = React.Children.map(children, (child) => {
+    if (!React.isValidElement<React.HTMLAttributes<HTMLElement>>(child)) return child;
+
+    return React.cloneElement(child, {
+      ...child.props,
+      className: cls(["keen-slider__slide", child.props.className]),
+    });
+  });
+
   return (
-    <div ref={container_ref} className={cls(["keen-slider", css.slider, className])}>
-      {children}
+    <div ref={container_ref} className={cls(["keen-slider", css.slider, className])} {...rest}>
+      {cloned_children}
     </div>
   );
 }
@@ -27,5 +36,3 @@ type SlidesOption = {
   perView?: number | "auto" | (() => number | "auto");
   spacing?: number | (() => number);
 };
-
-export const KSS = "keen-slider__slide";
--- src/views/IndexView/TopAnimeIsland/TopAnime/TopAnime.tsx
+++ src/views/IndexView/TopAnimeIsland/TopAnime/TopAnime.tsx
@@ -5,7 +5,7 @@ import Loading from "@react/components/loaders/Loading/Loading";
 import ErrorComp from "@react/components/errors/ErrorComp/ErrorComp";
 import { cls } from "@utils/cls";
 import { ANIME } from "@views/AnimeView/config";
-import KeenSlider, { KSS } from "@react/components/shared/KeenSlider/KeenSlider";
+import KeenSlider from "@react/components/shared/KeenSlider/KeenSlider";
 
 export default function TopAnime() {
   const query = useQuery({
@@ -13,22 +13,22 @@ export default function TopAnime() {
     queryFn: get_top_anime_fetch,
   });
 
+  const slide = query?.data?.data.map((item) => (
+    <div key={item.mal_id} className={cls([css.slide])}>
+      <a href={`${ANIME.href}?id=${item.mal_id}`}>
+        <img src={item.images.webp.large_image_url} alt={item.title} />
+      </a>
+      <p>{item.genres.map((genre) => genre.name).join(", ")}</p>
+    </div>
+  ));
+
   return (
     <div className={css.top_anime}>
       {query.isError && <ErrorComp />}
       {query.isLoading && <Loading />}
 
       {query.isSuccess && (
-        <KeenSlider options={{ perView: "auto", spacing: 32 }}>
-          {query.data.data.map((item) => (
-            <div key={item.mal_id} className={cls([KSS, css.slide])}>
-              <a href={`${ANIME.href}?id=${item.mal_id}`}>
-                <img src={item.images.webp.large_image_url} alt={item.title} />
-              </a>
-              <p>{item.genres.map((genre) => genre.name).join(", ")}</p>
-            </div>
-          ))}
-        </KeenSlider>
+        <KeenSlider options={{ perView: "auto", spacing: 32 }}>{slide}</KeenSlider>
       )}
     </div>
   );
