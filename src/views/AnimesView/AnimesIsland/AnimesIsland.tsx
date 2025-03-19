ff479d447be7b542bb8d15251ea1d8efbf306173
edard3v <edard3v@gmail.com>
2025-03-19 01:20:51 -0500

    estilando pagina donde muestro animes encontrados

--- src/views/AnimesView/AnimesIsland/AnimesIsland.tsx
+++ src/views/AnimesView/AnimesIsland/AnimesIsland.tsx
@@ -1,6 +1,7 @@
 import css from "./AnimesIsland.module.css";
 import { useStore } from "@nanostores/react";
 import { search_animes_store } from "@react/components/shared/SearchAnimesIsland/SearchAnimes/search_animes_store";
+import { ANIME } from "@views/AnimeView/config";
 
 export default function AnimesIsland() {
   const { animes } = useStore(search_animes_store.store);
@@ -9,10 +10,15 @@ export default function AnimesIsland() {
   return (
     <div className={css.animes}>
       {animes.data.map((item) => (
-        <div key={item.mal_id}>
-          <h2>{item.title}</h2>
-          <p>{item.synopsis}</p>
-          <img src={item.images.webp.large_image_url} alt={item.title} />
+        <div key={item.mal_id} className={css.card}>
+          <a href={`${ANIME.href}?id=${item.mal_id}`}>
+            <img src={item.images.webp.large_image_url} alt={item.title} />
+          </a>
+          <div className={css.txts}>
+            <h2>{item.title}</h2>
+            <p>{item.duration}</p>
+            <p>Estudios: {item.studios.map((item) => item.name).join(", ")}</p>
+          </div>
         </div>
       ))}
     </div>
